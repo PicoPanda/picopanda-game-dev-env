ea88ccd5b50872f2230a96b293fc5c681c88ceac
PlunderingPanda <carelandries@gmail.com>
2025-06-21 13:35:04 +0200

    Updated LUA API
    Made progress on integration of LUA VM and C engine.
    
    Added python scripts that takes the sprite sheet in C array format and converts it to .bin.
    
    For efficient access from the game engine side, the sprite sheet is embedded at a known location in the binary. (at the end of the script)
    
    This can be changed later to support multiple sprite sheets or something of the sorts, but for now this is a nice and clean solution.

--- renderingExample/renderingExample.lua
+++ renderingExample/renderingExample.lua
@@ -1,15 +1,27 @@
--- This variable will be global to the script
 counter = 0
 
 function game_logic_init()
-    print("Lua game_logic_init from C array!")
     counter = 0
 end
 
 function game_logic_loop()
     counter = counter + 1
-    print("Lua game_logic_loop from C array! Counter:", counter)
+
+    local buttons = get_button_status()
+
+    if buttons.up then
+        draw_string(0, 80, "UP pressed!", 5, 1, 0x0F)
+    end
+    
     draw_string(0, 10, "Counter: " .. tostring(counter), 5, 1, 0x0F)
-    draw_string(0, 20, "Louise is beeldskoon!", 5, 1, 0x0F)
-    draw_string(0, 30, "Bevan is n legende!", 5, 1, 0x0F)
+    draw_pixel(30, 30, 0x0F);
+    
+    draw_circle(63, 63, 15, true, 0x0F);
+
+    draw_line(0, 0, 127, 127, 0x0F);
+
+    draw_rectangle(0, 63, 10, 10, true, 0x0F);
+
+    draw_sprite( 32, 0, 32, 32, 2, 2, false, false)
+
 end
