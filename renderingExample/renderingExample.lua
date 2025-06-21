5a4f3749b0ca8ca2f81c03a708cda18e3c5304de
PlunderingPanda <carelandries@gmail.com>
2025-06-21 19:10:12 +0200

    Renames counter variable for clarity
    Updates the counter variable name to 'Counter' for better
    readability and consistency. It also updates the flip variable
    to 'Flip' for the same reason.

--- renderingExample/renderingExample.lua
+++ renderingExample/renderingExample.lua
@@ -1,12 +1,13 @@
-counter = 0
+Counter = 0
+Flip = false
 
 function game_logic_init()
-    counter = 0
-    flip = false
+    Counter = 0
+    Flip = false
 end
 
 function game_logic_loop()
-    counter = counter + 1
+    Counter = Counter + 1
 
     local buttons = get_button_status()
 
@@ -14,7 +15,7 @@ function game_logic_loop()
         draw_string(0, 80, "UP pressed!", FONT_4X5_FIXED, 1, 0x0F)
     end
 
-    draw_string(0, 10, "Counter: " .. tostring(counter), 5, 1, 0x0F)
+    draw_string(0, 10, "Counter: " .. tostring(Counter), 5, 1, 0x0F)
     draw_pixel(30, 30, 0x0F);
 
     draw_circle(63, 63, 15, true, 0x0F);
@@ -23,11 +24,11 @@ function game_logic_loop()
 
     draw_rectangle(0, 63, 10, 10, true, 0x0F);
 
-    if counter % 100 == 0 then
-        flip = not flip
+    if Counter % 100 == 0 then
+        Flip = not Flip
 
     end
 
-    draw_sprite(PIXELS_32x32, 0, 32, 32, 2, 2, flip, false)
+    draw_sprite(PIXELS_32x32, 0, 32, 32, 2, 2, Flip, false)
 
 end
